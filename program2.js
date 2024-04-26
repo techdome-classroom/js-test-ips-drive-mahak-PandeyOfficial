4e56a2f7f5f9bb02bd83d9f361f06f6f319b5d57
Mahak pandey <109313935+mahak-PandeyOfficial@users.noreply.github.com>
2024-04-26 13:39:12 +0530

    Update program2.js
    done

--- program2.js
+++ program2.js
@@ -1,7 +1,29 @@
+function differ( str, i, j){
+   var visit = new Array(256);
+
+   for(var k = i ; k<j ; k++){
+ if(visit[str.charAt(k) == true])
+    return false;
+    visit[str.charAt(k)] == true;
+    
+   }
+}
+
+
 function longestSubstring(s) {
-
-
+     var len = s.length;
+     var result = 0;
+     for(var i = 0; i<len; i++){
+        for(var j = i ; j<len ; j++){
+            if(differ(s, i, j)){
+                result = Math.max(result, j-i+1);
+                return result;
+            }
+        }
+     }
     // Implementation of longestSubstring function
 }
+
+
 
 module.exports = { longestSubstring };
